97b24cc0415824de661199fb300da93caebff4e8
Divya Debashrita Basantray <divyadebashrita1307@gmail.com>
2026-04-13 12:15:10 +0530

    Create data_checks.sql

---
+++ Scripts/silver/data_checks.sql
@@ -0,0 +1,37 @@
+-- CHECK FOR NULLS OR DUPLICATES IN PRIMARY KEY
+-- EXPECTATION : NO RESULT
+
+SELECT
+	prd_id,
+	COUNT(*)
+FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+-- CHECK FOR UNWANTED SPACES
+-- EXPEXTATION : NO RESULTS
+
+SELECT prd_nm
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm);
+
+-- CHECK FOR NULLS AND NEGATIVE NUMBERS
+-- EXPEXTATION : NO RESULTS
+
+SELECT prd_cost
+FROM silver.crm_prd_info
+WHERE prd_cost < 0 OR prd_cost IS NULL;
+
+-- DATA STANDARDIZATION AND CONSISTENCY
+
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info;
+
+-- CHECK FOR INVALID DATE ORDERS
+
+SELECT *
+FROM silver.crm_prd_info
+WHERE prd_end_dt < prd_start_dt;
+
+SELECT *
+FROM silver.crm_prd_info;
