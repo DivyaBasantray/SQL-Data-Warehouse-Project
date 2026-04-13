8de044a377cfef7e44d696babb722130e1c8ecdf
Divya Debashrita Basantray <divyadebashrita1307@gmail.com>
2026-04-13 12:16:30 +0530

    Update data_checks.sql

--- Scripts/bronze/data_checks.sql
+++ Scripts/bronze/data_checks.sql
@@ -35,3 +35,23 @@ WHERE prd_end_dt < prd_start_dt;
 
 SELECT *
 FROM bronze.crm_prd_info;
+
+SELECT	
+	prd_id,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,	--EXTRACT CATEGORY ID
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,			-- EXTRACT PRODUCT KEY
+	prd_nm,
+	ISNULL(prd_cost, 0) AS prd_cost,
+	CASE UPPER(TRIM(prd_line)) 
+		WHEN 'M' THEN 'Mountain'
+		WHEN 'R' THEN 'Road'
+		WHEN 'S' THEN 'Other Sales'
+		WHEN 'T' THEN 'Touring'
+		ELSE 'NA'
+	END AS prd_line,		-- MAP PRODUCT LINE CODES TO DESCRIPTIVE VALUES
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+	CAST(
+		LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt) -1 
+		AS DATE
+	) AS prd_end_dt			-- CALCULATE END DATE AS ONE DAY BEFORE THE NEXT START DATE
+FROM bronze.crm_prd_info;
