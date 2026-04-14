9ddcb06dc67ac6109afda6a95ec5405541f1a86f
Divya Debashrita Basantray <divyadebashrita1307@gmail.com>
2026-04-14 10:11:09 +0530

    Update data_checks.sql

--- Scripts/bronze/data_checks.sql
+++ Scripts/bronze/data_checks.sql
@@ -36,6 +36,8 @@ WHERE prd_end_dt < prd_start_dt;
 SELECT *
 FROM bronze.crm_prd_info;
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
 SELECT	
 	prd_id,
 	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,	--EXTRACT CATEGORY ID
@@ -55,3 +57,108 @@ SELECT
 		AS DATE
 	) AS prd_end_dt			-- CALCULATE END DATE AS ONE DAY BEFORE THE NEXT START DATE
 FROM bronze.crm_prd_info;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- CHECK FOR INVALID DATES
+
+SELECT sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0;
+
+SELECT 
+	NULLIF(sls_order_dt, 0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0 
+OR LEN(sls_order_dt) != 8 
+OR sls_order_dt > 20500101
+OR sls_order_dt <19000101;
+
+SELECT 
+	NULLIF(sls_due_dt, 0) sls_due_dt
+FROM bronze.crm_sales_details
+WHERE sls_due_dt <= 0 
+OR LEN(sls_due_dt) != 8 
+OR sls_due_dt > 20500101
+OR sls_due_dt <19000101;
+
+-- CHECK FOR INVALID DATE ORDERS
+
+SELECT *
+FROM bronze.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt
+OR sls_order_dt > sls_due_dt;
+
+-- CHECK DATA CONSISTENCY: BETWEEN SALES, QUANTITY, AND PRICE
+-- >> SLAES = QUANTITY * PRICE
+-- >> VALUES MUST NOT BE NULL, ZERO, OR NEGATIVE.
+
+SELECT DISTINCT
+	sls_sales,
+	sls_quantity,
+	sls_price
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price;
+
+--- RULES ---
+
+-- IF SALES IS NEGATIVE, XERO, OR NULL, DERIVE IT USING QUANTITY AND PRICE.
+-- IF PRICE IS ZERO OR NULL, CALCULATE IT USING SALES AND QUANTITY.
+-- IF PRICE IS NEGATIVE, CONVERT IT TO A POSITIVE VALUE.
+
+SELECT DISTINCT
+	sls_sales AS old_sls_sales,
+	sls_quantity,
+	sls_price AS old_sls_price,
+
+CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)  --ABS() : RETURNS ABSOLUTE VALUE OF A NUMBER
+	 THEN sls_quantity * ABS(sls_price)
+	 ELSE sls_sales
+END AS sls_sales,
+
+CASE WHEN sls_price IS NULL OR sls_price <= 0
+	 THEN sls_sales / NULLIF(sls_quantity, 0)
+	 ELSE sls_price
+END AS sls_price
+
+FROM bronze.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
+OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+ORDER BY sls_sales, sls_quantity, sls_price;
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+SELECT	
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+
+	CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		 ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END AS sls_order_dt,
+
+	CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+		 ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END AS sls_ship_dt,
+
+	CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+		 ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END AS sls_due_dt,
+	
+	CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)  --ABS() : RETURNS ABSOLUTE VALUE OF A NUMBER
+		 THEN sls_quantity * ABS(sls_price)
+		 ELSE sls_sales
+	END AS sls_sales,
+
+	sls_quantity,
+
+	CASE WHEN sls_price IS NULL OR sls_price <= 0
+		 THEN sls_sales / NULLIF(sls_quantity, 0)
+		 ELSE sls_price
+	END AS sls_price
+	
+FROM bronze.crm_sales_details;
