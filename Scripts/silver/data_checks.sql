1285ce522348a8847252a54e3bdf0d4e86a17910
Divya Debashrita Basantray <divyadebashrita1307@gmail.com>
2026-04-14 10:51:50 +0530

    Update data_checks.sql

--- Scripts/silver/data_checks.sql
+++ Scripts/silver/data_checks.sql
@@ -81,3 +81,19 @@ SELECT
 	) AS prd_end_dt			-- CALCULATE END DATE AS ONE DAY BEFORE THE NEXT START DATE
 FROM bronze.crm_prd_info;
 
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num			NVARCHAR(50),
+    sls_prd_key			NVARCHAR(50),
+    sls_cust_id         INT,
+    sls_order_dt        DATE,
+    sls_ship_dt			DATE,
+    sls_due_dt			DATE,
+    sls_sales			INT,
+    sls_quantity		INT,
+	sls_price			INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
