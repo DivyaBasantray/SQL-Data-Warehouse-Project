36ccad3b9ba7bbb950909d1e8f562b83152d663d
Divya Debashrita Basantray <divyadebashrita1307@gmail.com>
2026-04-15 18:52:52 +0530

    Update data_checks.sql

--- Scripts/silver/data_checks.sql
+++ Scripts/silver/data_checks.sql
@@ -143,3 +143,35 @@ FROM bronze.crm_sales_details;
 
 SELECT *
 FROM silver.crm_sales_details;
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- IDENTIFY OUT-OF-RANGE DATES
+
+SELECT DISTINCT bdate
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE();
+
+-- DATA STANDARDIZATION AND CONSISTENCY
+
+SELECT DISTINCT gen,
+FROM silver.erp_cust_az12;
+
+INSERT INTO silver.erp_cust_az12 (cid, bdate, gen)
+SELECT
+	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+		 ELSE cid
+	END AS cid,
+
+	CASE WHEN bdate > GETDATE() THEN NULL
+		 ELSE bdate
+	END AS bdate,
+	
+	CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+		 WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+		 ELSE 'NA'
+	END AS gen
+
+FROM bronze.erp_cust_az12;
+
+SELECT * FROM [silver].[crm_cust_info];
