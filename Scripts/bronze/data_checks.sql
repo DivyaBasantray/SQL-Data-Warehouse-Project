dd932c0696517a03c6462f0f2fd7c85957249317
Divya Debashrita Basantray <divyadebashrita1307@gmail.com>
2026-04-15 19:56:52 +0530

    Update data_checks.sql

--- Scripts/bronze/data_checks.sql
+++ Scripts/bronze/data_checks.sql
@@ -192,7 +192,28 @@ SELECT
 
 FROM bronze.erp_cust_az12;
 
+--------------------------------------------------------------------------------------------------------------------------------------------------------
 
+-- DATA STANDARDIZATION AND CONSISTENCY
+
+SELECT DISTINCT				
+		cntry AS old_cntry,
+		CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+			 WHEN TRIM(cntry) IN ('US', 'USA')  THEN 'United States'
+			 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'NA'
+			 ELSE TRIM(cntry)
+		END AS cntry	
+FROM bronze.erp_loc_a101
+ORDER BY cntry;
+
+SELECT 
+	REPLACE (cid,'-', '') cid,
+	CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		 WHEN TRIM(cntry) IN ('US', 'USA')  THEN 'United States'
+		 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'NA'
+		 ELSE TRIM(cntry)
+	END AS cntry
+FROM bronze.erp_loc_a101;
 
 
 
