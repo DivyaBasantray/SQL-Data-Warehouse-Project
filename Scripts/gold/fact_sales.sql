a5cb1ad0f159d486a9a334f70c98d2727d254d65
Divya Debashrita Basantray <divyadebashrita1307@gmail.com>
2026-04-20 14:22:24 +0530

    Create fact_sales.sql

---
+++ Scripts/gold/fact_sales.sql
@@ -0,0 +1,33 @@
+CREATE VIEW gold.fact_sales AS
+SELECT 
+	sd.sls_ord_num AS order_number,
+	pr.product_key,
+	cu.customer_key,
+	sd.sls_order_dt AS order_date,
+	sd.sls_ship_dt AS shipping_date,
+	sd.sls_due_dt AS due_date,
+	sd.sls_sales AS sales_amount,
+	sd.sls_quantity AS quantity,
+	sd.sls_price AS pice
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id;
+
+-- FOREIGN KEY INTEGRITY (DIMENSIONS)
+
+SELECT *
+FROM gold.fact_sales;
+
+SELECT *
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+WHERE c.customer_key IS NULL;
+
+SELECT *
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL;
